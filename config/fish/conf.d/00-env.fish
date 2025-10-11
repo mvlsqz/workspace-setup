82bf9f91003b751535bded588bc7d3f1a721efac
Marvin Velasquez <mvelasquez@paypal.com>
2025-10-11 10:35:29 -0600

    fix(fish): remove creds

--- config/fish/conf.d/00-env.fish
+++ config/fish/conf.d/00-env.fish
@@ -5,10 +5,3 @@ set -gx NVM_DIR (brew --prefix nvm)
 set -gx PATH $HOME/.rd/bin $PATH
 set -gx PATH $HOME/.local/bin $PATH
 set -gx PATH $HOME/local/bin $PATH
-
-# set -gx GITHUB_PERSONAL_ACCESS_TOKEN "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"
-# set -gx ANTHROPIC_AUTH_TOKEN "bd0b10a8ff3e0787d2a88cef7a93d63bf4cf8ee1bfbdbfc568bf27e3cbfea74f"
-# set -gx ANTHROPIC_BASE_URL "https://aiplatform.dev51.cbf.dev.paypalinc.com/cosmosai/llm/v1"
-# set -gx REQUESTS_CA_BUNDLE "/usr/local/etc/openssl/certs/combined_cacerts.pem"
-# set -gx SSL_CERT_FILE "/usr/local/etc/openssl/certs/combined_cacerts.pem"
-# set -gx NODE_EXTRA_CA_CERTS "/usr/local/etc/openssl/certs/paypal_proxy_cacerts.pem"
